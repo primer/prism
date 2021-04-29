b36f449383f2dcf050a401699123cccc4f70605b
Cole Bemis <colebemis@github.com>
2021-04-29 11:40:59 -0700

    Update index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,21 @@
 import { Link, RouteComponentProps } from "@reach/router";
 import React from "react";
 import { Button } from "../components/button";
+import { VStack } from "../components/stack";
 import { useGlobalState } from "../global-state";
 
 export function Index(props: RouteComponentProps) {
   const [state, send] = useGlobalState();
   return (
-    <div>
-      <ul>
+    <VStack spacing={16} style={{ padding: 16 }}>
+      <VStack as="ul" style={{ margin: 0, padding: 0, listStyle: "none" }}>
         {Object.values(state.context.palettes).map(palette => (
           <li key={palette.id}>
             <Link to={`/${palette.id}`}>{palette.name}</Link>
           </li>
         ))}
-      </ul>
+      </VStack>
       <Button onClick={() => send("CREATE_PALETTE")}>Create palette</Button>
-    </div>
+    </VStack>
   );
 }
