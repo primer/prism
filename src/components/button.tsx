c33f8c272b167b8314b206e4b1f249ba17161839
Cole Bemis <colebemis@github.com>
2022-05-25 16:58:10 -0700

    Extend primer button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,24 +1,39 @@
+import {
+  Button as PrimerButton,
+  IconButton as PrimerIconButton,
+} from "@primer/react";
 import styled from "styled-components";
 
-export const Button = styled.button`
-  height: 32px;
-  padding: 0 8px;
-  margin: 0;
-  font-family: inherit;
-  font-weight: 400;
-  font-size: 14px;
-  line-height: 1;
+export const Button = styled(PrimerButton)`
   color: var(--color-text);
-  background-color: var(--color-background-secondary, gainsboro);
-  border: 1px solid var(--color-border, darkgray);
-  border-radius: 3px;
-  appearance: none;
+  background-color: var(--color-background-secondary);
+  border: 1px solid var(--color-border);
+  box-shadow: none;
 
-  &:active {
-    background-color: var(--color-border, darkgray);
+  &:not([disabled]):hover,
+  &:not([disabled]):active {
+    background-color: var(--color-background-secondary-hover);
+    border-color: var(--color-border);
   }
 
   &[disabled] {
+    color: var(--color-text);
+    opacity: 0.5;
+  }
+`;
+
+export const IconButton = styled(PrimerIconButton)`
+  color: var(--color-text);
+  background-color: var(--color-background-secondary);
+  border: 1px solid var(--color-border);
+  box-shadow: none;
+
+  &:not([disabled]):hover {
+    background-color: var(--color-background-secondary-hover);
+  }
+
+  &[disabled] {
+    color: var(--color-text);
     opacity: 0.5;
   }
 `;
