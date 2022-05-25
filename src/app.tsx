c83b4b4768d52b13fd72a98eac3a729a41884d8f
Cole Bemis <colebemis@github.com>
2022-05-25 15:40:54 -0700

    Install @primer/react

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,21 @@
+import { BaseStyles, themeGet } from "@primer/react";
 import { Router } from "@reach/router";
 import React from "react";
 import { useHotkeys } from "react-hotkeys-hook";
+import { createGlobalStyle } from "styled-components";
+import { routePrefix } from "./constants";
 import { useGlobalState } from "./global-state";
 import { Index } from "./pages";
 import { Curve } from "./pages/curve";
 import { NotFound } from "./pages/not-found";
 import { Palette } from "./pages/palette";
 import { Scale } from "./pages/scale";
-import {routePrefix} from "./constants"
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    background-color: ${themeGet("colors.canvas.default")};
+  }
+`;
 
 export function App() {
   const [, send] = useGlobalState();
@@ -16,14 +24,17 @@ export function App() {
   useHotkeys("command+shift+z, ctrl+shift+z", () => send("REDO"));
 
   return (
-    <Router>
-      <Index path={`${routePrefix}/`} />
-      <Palette path={`${routePrefix}/:paletteId`}>
-        <Scale path="scale/:scaleId"></Scale>
-        <Scale path="scale/:scaleId/:index"></Scale>
-        <Curve path="curve/:curveId"></Curve>
-      </Palette>
-      <NotFound default />
-    </Router>
+    <BaseStyles>
+      <GlobalStyles />
+      <Router>
+        <Index path={`${routePrefix}/`} />
+        <Palette path={`${routePrefix}/:paletteId`}>
+          <Scale path="scale/:scaleId"></Scale>
+          <Scale path="scale/:scaleId/:index"></Scale>
+          <Curve path="curve/:curveId"></Curve>
+        </Palette>
+        <NotFound default />
+      </Router>
+    </BaseStyles>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,17 @@
-import { inspect } from "@xstate/inspect";
+import { ThemeProvider } from "@primer/react";
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./app";
-import "./index.css";
 import { GlobalStateProvider } from "./global-state";
-
-if (process.env.NODE_ENV === "development") {
-  inspect({ iframe: false });
-}
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStateProvider>
-      <App />
-    </GlobalStateProvider>
+    <ThemeProvider colorMode="auto">
+      <GlobalStateProvider>
+        <App />
+      </GlobalStateProvider>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
